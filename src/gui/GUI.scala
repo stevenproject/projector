299bdbc5e7a03e81811a228b21e5dc47988c6211
stevenproject <47649277+stevenproject@users.noreply.github.com>
2019-04-03 16:13:05 -0400

    Initial commit

---
+++ src/gui/GUI.scala
@@ -0,0 +1,70 @@
+package gui
+
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.{Group, Scene}
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.{Circle, Rectangle}
+
+object GUI extends JFXApp {
+
+  var OurPlayer: Group = new Group {}
+
+  val Player: Circle = new Circle {
+    centerX = Math.random() * 1280
+    centerY = Math.random() * 960
+    radius = 20
+    fill = Color.Red
+  }
+
+
+
+  val background = new Rectangle() {
+    width = 1280
+    height = 960
+    translateX = 0.0
+    translateY = 0.0
+    fill = Color.Yellow
+  }
+
+  this.stage = new PrimaryStage {
+    this.title = "fuck you"
+    scene = new Scene(1280, 960){
+      fill = Color.White
+      content = new HBox{
+        children = List(
+        new Rectangle{ //top
+          width = 700.0
+          height = 10.0
+          translateX = 0.0
+          translateY = 0.0
+          fill = Color.Black
+        },
+        new Rectangle{ //right
+          width = 10.0
+          height = 700.0
+          translateX = -700.0
+          translateY = 0.0
+          fill = Color.Black
+        },
+          new Rectangle{
+            width = 10.0
+            height = 700.0
+            translateX = -10.0
+            translateY = 0.0
+            fill = Color.Black
+          },
+          new Rectangle{
+            width = 715.0
+            height = 10.0
+            translateX = -725.0
+            translateY = 700.0
+          }
+        )
+      }
+    }
+  }
+
+
+}
