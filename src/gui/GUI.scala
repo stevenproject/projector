fcfca541575178a001370660d7b7f65d69c30ac4
Your lord and savior <squan@buffalo.edu>
2019-04-03 17:59:55 -0400

    ur mom

--- src/gui/GUI.scala
+++ src/gui/GUI.scala
@@ -2,6 +2,7 @@ package gui
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.effect.BlendMode.{Green, Red}
 import scalafx.scene.{Group, Scene}
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color
@@ -9,15 +10,44 @@ import scalafx.scene.shape.{Circle, Rectangle}
 
 object GUI extends JFXApp {
 
-  var OurPlayer: Group = new Group {}
+  var scenestuff: Group = new Group {}
 
   val Player: Circle = new Circle {
-    centerX = Math.random() * 1280
-    centerY = Math.random() * 960
+    centerX = 200
+    centerY = 200
     radius = 20
     fill = Color.BlueViolet
   }
-
+  val bounds: List[Rectangle] = List(
+    new Rectangle{ //top
+      width = 700.0
+      height = 10.0
+      translateX = 0.0
+      translateY = 0.0
+      fill = Color.Black
+    },
+    new Rectangle{ //right
+      width = 10.0
+      height = 700.0
+      translateX = 690.0
+      translateY = 0.0
+      fill = Color.Black
+    },
+    new Rectangle{
+      width = 10.0
+      height = 700.0
+      translateX = 0.0
+      translateY = 0.0
+      fill = Color.Red
+    },
+    new Rectangle{
+      width = 700.0
+      height = 10.0
+      translateX = 0.0
+      translateY = 690.0
+      fill = Color.BlueViolet
+    }
+  )
   val background = new Rectangle() {
     width = 1280
     height = 960
@@ -25,44 +55,18 @@ object GUI extends JFXApp {
     translateY = 0.0
     fill = Color.Yellow
   }
-
+  def addbounds(bound: List[Rectangle]): Unit = {
+    for (bond <- bound) {
+      scenestuff.children.add(bond)
+    }
+  }
+    addbounds(bounds)
+    scenestuff.children.add(Player)
   this.stage = new PrimaryStage {
     this.title = "lord help us!"
     scene = new Scene(1280, 960){
       fill = Color.White
-      content = new HBox{
-        children = List(
-        new Rectangle{ //top
-          width = 700.0
-          height = 10.0
-          translateX = 0.0
-          translateY = 0.0
-          fill = Color.Black
-        },
-        new Rectangle{ //right
-          width = 10.0
-          height = 700.0
-          translateX = -700.0
-          translateY = 0.0
-          fill = Color.Black
-        },
-          new Rectangle{
-            width = 10.0
-            height = 700.0
-            translateX = -10.0
-            translateY = 0.0
-            fill = Color.Red
-          },
-          new Rectangle{
-            width = 715.0
-            height = 10.0
-            translateX = -725.0
-            translateY = 700.0
-            fill = Color.BlueViolet
-          },
-          OurPlayer
-        )
+      content = scenestuff
       }
     }
   }
-}
