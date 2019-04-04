c9f7eaaf6b312ec4a22f42e9e4a7af3d69539db6
Your lord and savior <squan@buffalo.edu>
2019-04-04 13:43:11 -0400

    ur mom

--- src/gui/GUI.scala
+++ src/gui/GUI.scala
@@ -1,72 +1,115 @@
 package gui
 
+import javafx.scene.input.{KeyCode, KeyEvent}
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.effect.BlendMode.{Green, Red}
 import scalafx.scene.{Group, Scene}
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Circle, Rectangle}
+import scalafx.scene.shape.{Circle, Rectangle, Shape}
+import scalafx.scene.text.Text
 
 object GUI extends JFXApp {
 
   var scenestuff: Group = new Group {}
-
+  var playerSpeedx = 10
+  var playerSpeedy = 10
   val Player: Circle = new Circle {
     centerX = 200
     centerY = 200
     radius = 20
-    fill = Color.BlueViolet
+    fill = Color.HotPink
   }
-  val bounds: List[Rectangle] = List(
-    new Rectangle{ //top
-      width = 700.0
-      height = 10.0
-      translateX = 0.0
-      translateY = 0.0
-      fill = Color.Black
-    },
-    new Rectangle{ //right
+  val top = new Rectangle{ //top
+    width = 700.0
+    height = 10.0
+    x = 0.0
+    y = 0.0
+    fill = Color.Black
+  }
+  val right = new Rectangle{ //right
       width = 10.0
       height = 700.0
-      translateX = 690.0
-      translateY = 0.0
+      x = 690.0
+      y = 0.0
       fill = Color.Black
-    },
-    new Rectangle{
+  }
+  val left = new Rectangle{
       width = 10.0
       height = 700.0
-      translateX = 0.0
-      translateY = 0.0
-      fill = Color.Red
-    },
-    new Rectangle{
+      x = 0.0
+      y = 0.0
+      fill = Color.Black
+  }
+  val bot = new Rectangle{
       width = 700.0
       height = 10.0
-      translateX = 0.0
-      translateY = 690.0
-      fill = Color.BlueViolet
-    }
-  )
-  val background = new Rectangle() {
-    width = 1280
-    height = 960
-    translateX = 0.0
-    translateY = 0.0
-    fill = Color.Yellow
+      x = 0.0
+      y = 690.0
+      fill = Color.Black
   }
-  def addbounds(bound: List[Rectangle]): Unit = {
-    for (bond <- bound) {
-      scenestuff.children.add(bond)
+  val defeat = new Text {
+    text = "Score for this problem: 0.0"
+    style = "-fx-font-size: 96 pt"
+    fill = Color.Black
+    x = 0.0
+    y = 350.0
+  }
+  def addbounds(bound: Rectangle): Unit = {
+    scenestuff.children.add(bound)
+  }
+
+  scenestuff.children.add(Player)
+  addbounds(top)
+  addbounds(right)
+  addbounds(left)
+  addbounds(bot)
+  scenestuff.children.add(defeat)
+  def moveRight(): Unit = {
+    Player.translateX.value += playerSpeedx
+      if(Player.intersects(right.getBoundsInLocal)){
+        scenestuff.getChildren.remove(Player)
+        scenestuff.children.add(defeat)
+      }
+    }
+  def moveLeft(): Unit = {
+    Player.translateX.value -= playerSpeedx
+    if(Player.intersects(left.getBoundsInLocal)){
+      scenestuff.getChildren.remove(Player)
+      scenestuff.children.add(defeat)
     }
   }
-    addbounds(bounds)
-    scenestuff.children.add(Player)
+  def moveBot(): Unit = {
+    Player.translateY.value += playerSpeedx
+    if(Player.intersects(bot.getBoundsInLocal)){
+      scenestuff.getChildren.remove(Player)
+    }
+  }
+  def moveTop(): Unit = {
+    Player.translateY.value -= playerSpeedx
+    if(Player.intersects(top.getBoundsInLocal)){
+      playerSpeedx = 0
+      playerSpeedy = 0
+      scenestuff.getChildren.remove(Player)
+    }
+  }
+  def keyPressed(keyCode: KeyCode): Unit = {
+    keyCode.getName match {
+      case "W" => moveTop()
+      case "A" => moveLeft()
+      case "S" => moveBot()
+      case "D" => moveRight()
+      case _ => println(keyCode.getName + " pressed with no action")
+    }
+  }
+
   this.stage = new PrimaryStage {
     this.title = "lord help us!"
     scene = new Scene(1280, 960){
       fill = Color.White
       content = scenestuff
       }
+    addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed(event.getCode))
     }
   }
