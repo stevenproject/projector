7cede8818c2b9e582551cbf20ae3081e1034a146
Your lord and savior <squan@buffalo.edu>
2019-04-04 16:22:45 -0400

    ur mom

--- src/gui/GUI.scala
+++ src/gui/GUI.scala
@@ -1,13 +1,13 @@
 package gui
 
 import javafx.scene.input.{KeyCode, KeyEvent}
+import javafx.scene.input.MouseEvent
+import scalafx.animation.AnimationTimer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.effect.BlendMode.{Green, Red}
 import scalafx.scene.{Group, Scene}
-import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Circle, Rectangle, Shape}
+import scalafx.scene.shape.{Circle, Rectangle}
 import scalafx.scene.text.Text
 
 object GUI extends JFXApp {
@@ -21,33 +21,33 @@ object GUI extends JFXApp {
     radius = 20
     fill = Color.HotPink
   }
-  val top: Rectangle = new Rectangle{
+  val top: Rectangle = new Rectangle {
     width = 1024.0
     height = 10.0
     x = 0.0
     y = 100.0
     fill = Color.Black
   }
-  val right: Rectangle = new Rectangle{
-      width = 10.0
-      height = 768.0
-      x = 1014.0
-      y = 100.0
-      fill = Color.Black
+  val right: Rectangle = new Rectangle {
+    width = 10.0
+    height = 768.0
+    x = 1014.0
+    y = 100.0
+    fill = Color.Black
   }
-  val left: Rectangle = new Rectangle{
-      width = 10.0
-      height = 768.0
-      x = 0.0
-      y = 100.0
-      fill = Color.Black
+  val left: Rectangle = new Rectangle {
+    width = 10.0
+    height = 768.0
+    x = 0.0
+    y = 100.0
+    fill = Color.Black
   }
-  val bot: Rectangle = new Rectangle{
-      width = 1024.0
-      height = 10.0
-      x = 0.0
-      y = 858.0
-      fill = Color.Black
+  val bot: Rectangle = new Rectangle {
+    width = 1024.0
+    height = 10.0
+    x = 0.0
+    y = 858.0
+    fill = Color.Black
   }
   val defeat: Text = new Text {
     text = "Score for this problem: 0.0"
@@ -56,6 +56,7 @@ object GUI extends JFXApp {
     x = 0.0
     y = 100.0
   }
+
   def addbounds(bound: Rectangle): Unit = {
     scenestuff.children.add(bound)
   }
@@ -68,33 +69,37 @@ object GUI extends JFXApp {
 
   def moveRight(): Unit = {
     Player.translateX.value += playerSpeedx
-      if(Player.getBoundsInParent.intersects(right.getBoundsInLocal)){
-        scenestuff.getChildren.remove(Player)
-        scenestuff.children.add(defeat)
-      }
+    if (Player.getBoundsInParent.intersects(right.getBoundsInLocal)) {
+      scenestuff.getChildren.remove(Player)
+      scenestuff.children.add(defeat)
     }
+  }
+
   def moveLeft(): Unit = {
     Player.translateX.value -= playerSpeedx
-    if(Player.getBoundsInParent.intersects(left.getBoundsInParent)){
+    if (Player.getBoundsInParent.intersects(left.getBoundsInParent)) {
       scenestuff.getChildren.remove(Player)
       scenestuff.children.add(defeat)
     }
   }
+
   def moveBot(): Unit = {
     Player.translateY.value += playerSpeedx
-    if(Player.getBoundsInParent.intersects(bot.getBoundsInParent)){
+    if (Player.getBoundsInParent.intersects(bot.getBoundsInParent)) {
       scenestuff.getChildren.remove(Player)
       scenestuff.children.add(defeat)
     }
   }
+
   def moveTop(): Unit = {
     Player.translateY.value -= playerSpeedx
-    if(Player.getBoundsInParent.intersects(top.getBoundsInParent)){
+    if (Player.getBoundsInParent.intersects(top.getBoundsInParent)) {
       scenestuff.getChildren.remove(Player)
       scenestuff.children.add(defeat)
     }
   }
-  def keyPressed(keyCode: KeyCode): Unit = {
+
+  def keyboardMove(keyCode: KeyCode): Unit = {
     keyCode.getName match {
       case "W" => moveTop()
       case "A" => moveLeft()
@@ -103,13 +108,44 @@ object GUI extends JFXApp {
       case _ => println("wrong button sir")
     }
   }
+  def MouseMovex (x: Double): Unit = {
+    if (Player.translateX.value < x) {
+      Player.translateX.value += x/100
+      MouseMovex(x)
+    }
+    else if (Player.translateX.value > x) {
+      Player.translateX.value -= x/100
+      MouseMovex(x)
+    }
+  }
+    def MouseMovey (y: Double): Unit = {
+      if (Player.translateY.value < y) {
+        Player.translateY.value += y/100
+        MouseMovey(y)
+      }
+      else if (Player.translateY.value > y) {
+        Player.translateY.value -= y/100
+        MouseMovey(y)
+      }
+  }
+  def MouseMove(x: Double, y: Double): Unit = {
+    MouseMovex(x)
+    MouseMovey(y)
+  }
 
   this.stage = new PrimaryStage {
     this.title = "Submit to Autograder"
-    scene = new Scene(1280, 960){
+    scene = new Scene(1280, 960) {
       fill = Color.White
       content = scenestuff
-      }
-    addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed(event.getCode))
     }
+    addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyboardMove(event.getCode))
+    addEventHandler(MouseEvent.MOUSE_CLICKED, (event: MouseEvent) => MouseMove(event.getX, event.getY))
   }
+  val update: Long => Unit = (time: Long) => {
+    4
+  }
+
+  // Start Animations. Calls update 60 times per second (takes update as an argument)
+  AnimationTimer(update).start()
+}
